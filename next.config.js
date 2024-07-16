373a25f15aaa572a23c64b786dd14b2e399d8d03
cakfan <taufan.fhm8@gmail.com>
2024-07-16 18:40:14 +0700

    feat: add PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
---
+++ src/app/(routes)/offline/page.tsx
@@ -0,0 +1,7 @@
+export default function OfflinePage() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">You're offline</h1>
+    </div>
+  );
+}
